423ba2732a501e4adef70ab03d55b697efb0c587
mzeeshanaltaf <154883001+mzeeshanaltaf@users.noreply.github.com>
2024-05-26 20:18:27 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+from streamlit_option_menu import option_menu
+from utility import *
+
+# --- PAGE SETUP ---
+# Initialize streamlit app
+page_title = "AI Nutritionist"
+page_icon = "🥗"
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout="centered")
+
+# Display the lottie file
+display_lottiefile("nutrition.json")
+
+st.title("Personal AI Nutritionist")
+st.write("***See Your Food, Know Your Fuel: Calorie Conscious at a Glance!***")
+st.write("***Your Personal AI Nutritionist*** 🥩🥦🥝")
+
+# ---- NAVIGATION MENU -----
+selection = option_menu(
+    menu_title=None,
+    options=["Analyze", "Recipe", "Diet Planner", "About"],
+    icons=["bi-boxes", "book", "body-text", "app"],  # https://icons.getbootstrap.com
+    orientation="horizontal",
+)
+
+# --- SIDEBAR SETUP ---
+# Configure the API key
+st.sidebar.title("Config️uration Options")
+api_key = sidebar_api_key_configuration()
+
+if selection == 'Analyze':
+    uploaded_file = sidebar_image_uploader()
+    st.header("Analyze Food")
+    st.write("Upload the food image to get the details of food items and caloric intake.")
+    submit_analyze = st.button("Analyze", type="primary", disabled=not uploaded_file)
+
+    if submit_analyze:
+        with st.spinner("Analyzing ..."):
+            response = get_gemini_response_image(uploaded_file, selection, api_key)
+            st.write(response)
+
+# If selected menu option is "Recipe"
+if selection == 'Recipe':
+    uploaded_file = sidebar_image_uploader()
+    st.header("Food Recipe")
+    st.write("Upload the food image and get the recipe.")
+    submit_recipe = st.button("Get Recipe", type="primary", disabled=not uploaded_file)
+
+    if submit_recipe:
+        with st.spinner("Generating Recipe..."):
+            response = get_gemini_response_image(uploaded_file, selection, api_key)
+            st.write(response)
+
+# If selected menu option is "Diet Planner"
+if selection == 'Diet Planner':
+    diet_options, calorie_intake = sidebar_food_calorie_configuration()
+    diet_plan = f"List of food items: {diet_options}. Number of calories per day: {calorie_intake}"
+    st.header("Diet Plan")
+    st.write("Select the food items and calorie intake to get the customized diet plan.")
+
+    diet_plan_submit = st.button("Get Diet Plan", type="primary", disabled=not diet_options)
+    if diet_plan_submit:
+        with st.spinner("Generating Diet Plan..."):
+            response = get_gemini_response(diet_plan, api_key)
+            st.write(response)
+
+# If selected menu option is "About"
+if selection == "About":
+    with st.expander("About this App"):
+        st.markdown(''' This app has following functionality:
+
+    - Get the details of food items and caloric intake by uploading food image.
+    - Get the recipie of uploaded food image
+    - Make the customized diet plan
+
+        ''')
+    with st.expander("Which Large Language models are supported by this App?"):
+        st.markdown(''' This app supports following multimodal:
+
+    * Google -- gemini-1.5-pro-latest
+
+        ''')
+    with st.expander("Where to get the source code of this app?"):
+        st.markdown(''' Source code is available at:
+    -  https://github.com/mzeeshanaltaf/genai-rag-groq-faiss
+        ''')
+
+    with st.expander("Whom to contact regarding this app?"):
+        st.markdown(''' Contact [Zeeshan Altaf](zeeshan.altaf@gmail.com)
+        ''')
---
+++ utility.py
@@ -0,0 +1,128 @@
+import google.generativeai as genai
+from streamlit_lottie import st_lottie
+import streamlit as st
+import json
+from PIL import Image
+
+input_prompt_analyze = """
+You are an expert nutritionist where you need to see the food items from the image and
+calculate the total calories. Also provide the details of every food items with calories intake
+in tabular format with column names as 'Items list', 'Calories' and each row contains details
+of items and calories in their respective columns
+
+Finally, you can also mention whether the food is healthy or not and also mention the
+approximate percentage split of the ratio of carbohydrates, fats, fibers, sugar and other important things
+required in our diet in tabular format.
+
+In case if the image is not related to food item, mention that and don't take any further steps
+"""
+
+input_prompt_recipe = """
+You are an expert nutritionist where you need to see the food items from the image and
+will share the food recipe step by step.
+
+In case if the image is not related to food item, mention that and don't take any further steps.
+"""
+
+input_prompt_diet = """
+You are an expert nutritionist. Purpose a diet plan based on given inputs of food items and per day caloric intake. 
+Diet plan should include diet for Breakfast, Lunch and Dinner and should include only the food items provided as input.
+Also, caloric intake should not exceed the per day calorie provided as input.
+
+"""
+
+if "prompt_activation" not in st.session_state:
+    st.session_state.prompt_activation = False
+
+
+# Function for API configuration at sidebar
+def sidebar_api_key_configuration():
+    st.sidebar.subheader("API Keys")
+    api_key = st.sidebar.text_input("Enter your API Key 🗝️", type="password",
+                                    help='Get API Key from: https://aistudio.google.com/app/apikey')
+    if api_key == '':
+        st.sidebar.warning('Enter the API Key 🗝️')
+        st.session_state.prompt_activation = False
+    elif api_key.startswith('AI') and (len(api_key) == 39):
+        st.sidebar.success('Lets Proceed!', icon='️👉')
+        st.session_state.prompt_activation = True
+    else:
+        st.sidebar.warning('Please enter the correct API Key 🗝️!', icon='⚠️')
+        st.session_state.prompt_activation = False
+    return api_key
+
+
+# Function to select diet options
+def sidebar_food_calorie_configuration():
+    st.sidebar.subheader("Diet Options")
+    diet_options = st.sidebar.multiselect('Select the Food items', ['Fruits', 'Vegetables', 'Lentils', 'Eggs', 'Meat'],
+                                          disabled=not st.session_state.prompt_activation)
+    calorie_intake = st.sidebar.number_input('Enter the Calorie Intake', value=1000, min_value=500, max_value=5000,
+                                             step=50)
+    return diet_options, calorie_intake
+
+
+# Function for image loader configuration at sidebar
+def sidebar_image_uploader():
+    st.sidebar.header("Upload Image")
+    uploaded_file = st.sidebar.file_uploader("Upload an image of Food", type=["jpg", "jpeg", "png"],
+                                             disabled=not st.session_state.prompt_activation)
+    image = ""
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.sidebar.image(image, caption="Uploaded Image", use_column_width=True)
+    return uploaded_file
+
+
+# Function to load and display the lottie file
+def display_lottiefile(filename):
+    # Load the lottie file
+    with open(filename, "r") as f:
+        lottie_file = json.load(f)
+    st_lottie(lottie_file, speed=1, reverse=False, loop=True, quality="high", height=200, width=400, key=None)
+
+
+# Function to get the list of available gemini models
+def get_gemini_model_list():
+    model_list = []
+    for m in genai.list_models():
+        if 'generateContent' in m.supported_generation_methods:
+            model_list.append(m.name)
+    return model_list
+
+
+# Function to convert the image into format support by gemini model
+def input_image_setup(uploaded_file):
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+
+# Function to get the response from gemini model with image as the input
+def get_gemini_response_image(uploaded_file, selection, api_key):
+    genai.configure(api_key=api_key)
+    image = input_image_setup(uploaded_file)
+    model = genai.GenerativeModel('gemini-1.5-pro-latest')
+    input_prompt = ''
+    if selection == 'Analyze':
+        input_prompt = input_prompt_analyze
+    elif selection == 'Recipe':
+        input_prompt = input_prompt_recipe
+    response = model.generate_content([input_prompt, image[0]])
+    return response.text
+
+
+# Function to get the response from gemini model with text as the input
+def get_gemini_response(diet_plan, api_key):
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel('gemini-1.5-pro-latest')
+    response = model.generate_content([input_prompt_diet, diet_plan])
+    return response.text
